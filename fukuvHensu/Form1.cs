fe124b6b25bb29672cb6622e5ce3e51c7697bc96
A601-22 <tnk.dat.class@gmail.com>
2021-06-10 10:16:35 +0900

    v

--- fukuvHensu/Form1.cs
+++ fukuvHensu/Form1.cs
@@ -12,6 +12,8 @@ namespace fukuvHensu
 {
     public partial class Form1 : Form
     {
+        int vx = 0;
+        int vy = 0;
         public Form1()
         {
             InitializeComponent();
@@ -19,32 +21,32 @@ namespace fukuvHensu
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Left += int.Parse(textBox1.Text);
-            label1.Top += int.Parse(textBox2.Text);
+            label1.Left += vx;
+            label1.Top += vy;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "0";
-            textBox2.Text = "-1";
+            vx= 0;
+            vy = -1;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "-1";
-            textBox2.Text = "0";
+            vx = -1;
+            vy = 0;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "1";
-            textBox2.Text = "0";
+            vx = 1;
+            vy = 0;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "0";
-            textBox2.Text = "1";
+            vx = 0;
+            vy = 1;
         }
     }
 }
